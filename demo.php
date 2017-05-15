5c662f07de27a38c45d677857f7cd0597abcb6b5
netfox01 <eissa.soubhi@gmail.com>
2017-05-15 14:35:19 +0200

    updated demo

--- demo.php
+++ demo.php
@@ -7,22 +7,17 @@ class Person extends PhpDot
     public $name = "eissa",
             $age = 25;
 
-    function __construct()
-    {
-        parent::__construct();
-    }
-
 }
 
 
 $person = new Person;
 
 echo $person.name; // eissa
-//or
-echo $person.$name; // eissa
-
 echo "<br>";
-
 echo $person.age; // 25
-// or
+
+echo "<br> or <br>";
+
+echo $person.$name; // eissa
+echo "<br>";
 echo $person.$age; // 25
